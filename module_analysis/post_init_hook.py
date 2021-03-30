c546af4d6b9b721b77d7b066e31582390c77e1fb
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-03-30 11:32:04 +0200

    [FIX+IMP] module_analysis : remove analysis when updating modules, because the analysis is partial (it also make the update slower) ; Add instead a cron task that is executed nightly to update analysis automatically

--- module_analysis/post_init_hook.py
+++ module_analysis/post_init_hook.py
@@ -7,5 +7,7 @@ from odoo import SUPERUSER_ID, api
 
 def analyse_installed_modules(cr, registry):
     env = api.Environment(cr, SUPERUSER_ID, {})
-    installed_modules = env["ir.module.module"].search([("state", "=", "installed")])
+    installed_modules = env["ir.module.module"].search(
+        ["|", ("state", "=", "installed"), ("name", "=", "module_analysis")]
+    )
     installed_modules.button_analyse_code()
